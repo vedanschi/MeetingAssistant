61c173fc06304b57077fb342675ebbb3ae93c3b2
samruddhitiwari <121943400+samruddhitiwari@users.noreply.github.com>
2025-05-11 20:37:10 +0530

    Added setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MeetingAssistant',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'PySimpleGUI',
+        'llama-cpp-python',
+        'faiss-cpu',  # or 'faiss-gpu'
+        'python-telegram-bot',
+    ],
+    author='Samruddhi Tiwari',  
+    author_email='samruddhitiwari003@gmail.com',  
+    description='A conversational meeting assistant',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/samruddhitiwari/MeetingAssistant',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
